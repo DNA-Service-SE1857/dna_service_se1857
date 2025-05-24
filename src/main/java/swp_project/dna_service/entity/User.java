f72ceaedbc959163b33ac62db2eda1c6ee62933d
Fatnotfatt <phatvttse171823@fpt.edu.vn>
2025-05-24 09:58:34 +0700

    Resolve conflict

---
+++ src/main/java/swp_project/dna_service/entity/User.java
@@ -0,0 +1,22 @@
+package swp_project.dna_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id ;
+    String username;
+
+
+}
