4d9964d615e5da1aaac3ab368dc0c6dba6b93a20
Fatnotfatt <phatvttse171823@fpt.edu.vn>
2025-05-26 12:48:54 +0700

    Docker file + Docker image public hub

---
+++ src/main/java/swp_project/dna_service/configuration/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package swp_project.dna_service.configuration;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info().title("DNA Service API")
+                        .description("API Documentation for DNA Service")
+                        .version("1.0"));
+    }
+}
