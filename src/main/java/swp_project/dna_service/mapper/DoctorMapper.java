00d663c3ad9c0fe033c7cce95e9293b2714bc2f7
Vo Truong Thanh Phat <phatvttse171823@fpt.edu.vn>
2025-06-24 18:58:57 +0700

    fix bug v2

--- src/main/java/swp_project/dna_service/mapper/DoctorMapper.java
+++ src/main/java/swp_project/dna_service/mapper/DoctorMapper.java
@@ -1,8 +1,10 @@
 package swp_project.dna_service.mapper;
 
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import swp_project.dna_service.dto.request.DoctorRequest;
 import swp_project.dna_service.dto.response.DoctorResponse;
 import swp_project.dna_service.entity.Doctor;
@@ -12,6 +14,7 @@ public interface DoctorMapper {
 
     Doctor toDoctor(DoctorRequest request);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateDoctor (@MappingTarget Doctor doctor, DoctorRequest request);
 
     DoctorResponse toDoctorResponse(Doctor doctor);
