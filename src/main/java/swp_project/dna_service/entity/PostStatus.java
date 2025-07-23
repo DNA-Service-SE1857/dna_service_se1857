3faafd40a89fde2b978fb72ca1bf7deab97a0376
Fatnotfatt <phatvttse171823@fpt.edu.vn>
2025-07-23 07:58:26 +0700

    fix field content thành text

--- src/main/java/swp_project/dna_service/entity/PostStatus.java
+++ src/main/java/swp_project/dna_service/entity/PostStatus.java
@@ -19,6 +19,8 @@ public class PostStatus {
     @GeneratedValue(strategy = GenerationType.UUID)
     String id;
     String title ;
+
+    @Column(columnDefinition = "TEXT")
     String content;
     String imageUrl;
 
